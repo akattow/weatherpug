139ed99d698c8798dab4a0d49660f878381e8561
Kat Tow <kattow88@gmail.com>
2019-03-13 20:18:21 -0500

    minor updates

--- js/index.js
+++ js/index.js
@@ -10,20 +10,18 @@ var weatherPhoto = {
 };
 
 var weatherQuote = {
-	cloudy: '“Cloudy with a chance of staying in all night.”',
-	sunny: '“My rolls look good in the sun”',
-	cold: '“Me when it\'s cold out”',
+	cloudy: "“Cloudy with a chance of staying in all night.”",
+	sunny: "“My rolls look good in the sun”",
+	cold: "“Me when it's cold out”",
 	snowy: '"The snow brings out my inner diva”',
-	rainy: '“Why does it always have to rain when I\'m having a good hair day”',
-	foggy: '“Of course I did my hair today‬”',
-	panic: '“Can we just stay in and cuddle?”',
-	sleepy: '“If u love me let me sleep”'
+	rainy: "“Why does it always have to rain when I'm having a good hair day”",
+	foggy: "“Of course I did my hair today‬”",
+	panic: "“Can we just stay in and cuddle?”",
+	sleepy: "“If u love me let me sleep”"
 };
 
-
-
 function getWeather() {
-	$.getJSON("https://ipinfo.io/?callback", function(x) {
+	$.getJSON("https://ipinfo.io/", function(x) {
 		var location = x.loc;
 		var splitLoc = location.split(",");
 		var latitude = Number(Number(splitLoc[0]).toFixed(2));
@@ -35,79 +33,84 @@ function getWeather() {
 
 		$("#location").html(city + ", " + state + "  |  ");
 
-			var apiUrl = "https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?lat=%lat%&lon=%long%&APPID=527a4c21b597169e7cec5fce156297ef";
-			var urlLat = apiUrl.replace("%lat%", latitude);
-			var urlLatLong = urlLat.replace("%long%", longitude);
+		var apiUrl =
+			"https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?lat=%lat%&lon=%long%&APPID=527a4c21b597169e7cec5fce156297ef";
+		var urlLat = apiUrl.replace("%lat%", latitude);
+		var urlLatLong = urlLat.replace("%long%", longitude);
 
-			$.getJSON(urlLatLong, function(y) {
-				var weatherDescription = y.weather[0].description;
+		$.getJSON(urlLatLong, function(y) {
+			var weatherDescription = y.weather[0].description;
 
-				$("#description").html(weatherDescription);
+			$("#description").html(weatherDescription);
 
-				tempKelvin = y.main.temp;
-				tempFahrenheit = Math.round(Number((((9 * (tempKelvin - 273.15)) /5) + 32).toFixed(2)));
-				tempCelsius = Math.round(Number((((tempFahrenheit - 32) * 5) / 9).toFixed(2)));
+			tempKelvin = y.main.temp;
+			tempFahrenheit = Math.round(
+				Number((9 * (tempKelvin - 273.15) / 5 + 32).toFixed(2))
+			);
+			tempCelsius = Math.round(Number(((tempFahrenheit - 32) * 5 / 9).toFixed(2)));
 
-				$("#c").html(tempCelsius + "°C");
-				$("#f").html(tempFahrenheit + "°F");
-				
-				if (x.country === "US") {
-					$("#f").show();
-					$("#c").hide();
-				} else {
-					$("#f").hide();
-					$("#c").show();
-				}
+			$("#c").html(tempCelsius + "°C");
+			$("#f").html(tempFahrenheit + "°F");
 
-				function changePug(z) {
-					$("#dougPic").css("background-image", "url(" + weatherPhoto[z] + ")");
-					$("#dougQuote").html(weatherQuote[z]);
-				}
+			if (x.country === "US") {
+				$("#f").show();
+				$("#c").hide();
+			} else {
+				$("#f").hide();
+				$("#c").show();
+			}
 
-				var id = y.weather[0].id.toString();
-				var date = new Date()
-				var hour = date.getHours();
-				
-				if (hour <= 7 || hour >= 19) {
-					changePug("sleepy");
-				} else {
-					switch (id[0]) {
-						case "6":
-							changePug("snowy");
-							break;
-						case "7":
-							if (id[2] == 8) {
-								changePug("panic");
-							} else {
-								changePug("foggy");
-							}
-							break;
-						case "8":
-							if ( id[2] != 0) {
-								changePug("cloudy");
-							} else {
-								changePug("sunny");
-							}
-							break;
-						case "9":
-							if (id[2] == 3) {
-								changePug("cold");
-							} else if (id[2] == 4) {
-								changePug("sunny");
-							} else {
-								changePug("panic");
-							}
-							break;
-						default:
-							changePug("rainy");
-							break;
-					} //switch	
-				}
-			}); //weather api call
+			function changePug(z) {
+				$("#dougPic").css("background-image", "url(" + weatherPhoto[z] + ")");
+				$("#dougQuote").html(weatherQuote[z]);
+			}
+
+			var id = y.weather[0].id.toString();
+			var date = new Date();
+			var hour = date.getHours();
+
+			if (hour <= 7 || hour >= 19) {
+				changePug("sleepy");
+			} else {
+				switch (id[0]) {
+					case "6":
+						changePug("snowy");
+						break;
+					case "7":
+						if (id[2] == 8) {
+							changePug("panic");
+						} else {
+							changePug("foggy");
+						}
+						break;
+					case "8":
+						if (id[2] != 0) {
+							changePug("cloudy");
+						} else {
+							changePug("sunny");
+						}
+						break;
+					case "9":
+						if (id[2] == 3) {
+							changePug("cold");
+						} else if (id[2] == 4) {
+							changePug("sunny");
+						} else {
+							changePug("panic");
+						}
+						break;
+					default:
+						changePug("rainy");
+						break;
+				} //switch
+			}
+		}); //weather api call
 	}); //ip call
 } //getWeather
 
 getWeather();
+
+//C to F || F to C
 
 $("#c").click(function() {
 	$("#c").hide();
@@ -128,3 +131,5 @@ $("#dougHover").mouseleave(function() {
 	$("#dougPic").show();
 	$("#dougHover").hide();
 });
+
+console.log(getWeather());
